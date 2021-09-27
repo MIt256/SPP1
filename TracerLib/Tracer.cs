8beb07373459012a6a2d46988d78017e6def69c9
Maks <xxzanko@gmail.com>
2021-09-27 19:10:58 +0300

    folders

---
+++ TracerLib/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TracerFolder
+{
+    public class Tracer : ITracer
+    {
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult();
+        }
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
